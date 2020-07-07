fb68619fb246b04b0bba55d6a2b73ca7c89c2815
Kanwar gill <gill.nihal@live.com>
2020-07-07 14:47:38 -0700

    Day-21_solutions

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
-'use strict';
+"use strict";
+const express = require("express");
+const morgan = require("morgan");
 
-const morgan = require('morgan');
+const { top50 } = require("./data/top50");
 
-const { top50 } = require('./data/top50');
+const q1 = (req, res) => {
+  res.render("pages/top50", {
+    title: "Top 50 Songs Streamed on Spotify",
+    top50,
+  });
+};
+
+const q2 = (req, res) => {
+  console.log(req.params.rank);
+  let rank = req.params.rank - 1;
+  if (top50[rank]) {
+    res.render("pages/songPage", {
+      title: `Song# ${top50[rank].rank}`,
+      song: top50[rank],
+    });
+  } else {
+    res.status(404);
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
+      path: req.originalUrl,
+    });
+  }
+};
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'ejs');
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 // endpoints here
-
+app.get("/top50", q1);
+app.get("/songPage/:rank", q2);
+// app.get("/top50/popular-artist",)
 // handle 404s
-app.git('*', (req, res) => {
-    res.status(404);
-    res.render('pages/fourOhFour', {
-        title: 'I got nothing',
-        path: req.originalUrl
-    });
+app.get("*", (req, res) => {
+  res.status(404);
+  res.render("pages/fourOhFour", {
+    title: "I got nothing",
+    path: req.originalUrl,
+  });
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
